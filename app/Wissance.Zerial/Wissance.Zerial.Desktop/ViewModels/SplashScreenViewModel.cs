6e4878dabefa939053b9d4ecd6e270a13c14a18d
Ushakov Michael <um.nix.user@gmail.com>
2024-02-11 18:17:51 +0500

    splash screen filling with content

--- app/Wissance.Zerial/Wissance.Zerial.Desktop/ViewModels/SplashScreenViewModel.cs
+++ app/Wissance.Zerial/Wissance.Zerial.Desktop/ViewModels/SplashScreenViewModel.cs
@@ -1,8 +1,19 @@
+using System;
+
 namespace Wissance.Zerial.Desktop.ViewModels
 {
 
     public class SplashScreenViewModel : ViewModelBase
     {
+        public string Years
+        {
+            get
+            {
+                int currentYear = DateTime.Now.Year;
+                return currentYear > YearOfWorksStarted ? $"{currentYear}-2023" : "2024-2023";
+            }
+        }
 
+        private const int YearOfWorksStarted = 2023;
     }
 }
