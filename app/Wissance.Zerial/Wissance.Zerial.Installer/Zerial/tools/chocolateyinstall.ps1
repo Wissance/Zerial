d017796ee89380cea35e7b1b0241f7f34acdf4f8
Ushakov Michael <um.nix.user@gmail.com>
2024-02-21 16:06:12 +0500

    slent install in choco

--- app/Wissance.Zerial/Wissance.Zerial.Installer/Zerial/tools/chocolateyinstall.ps1
+++ app/Wissance.Zerial/Wissance.Zerial.Installer/Zerial/tools/chocolateyinstall.ps1
@@ -17,7 +17,7 @@ $packageArgs = @{
   checksum64    = 'D2B49D854F19730E96D6AF2CA38CFA4C3D81CEC4E41FACB5A715809531F6550F'
   checksumType64= 'sha256'
 
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  silentArgs    = "/SILENT /qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
 }
 
