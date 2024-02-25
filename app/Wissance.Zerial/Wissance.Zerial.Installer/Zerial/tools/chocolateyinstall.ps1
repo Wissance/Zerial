9f745473094fe449c00f50fae6b8aacbd635fbea
Ushakov Michael <um.nix.user@gmail.com>
2024-02-25 22:33:25 +0500

    hashes of installer itself  were replaced with Bootstrapper hash

--- app/Wissance.Zerial/Wissance.Zerial.Installer/Zerial/tools/chocolateyinstall.ps1
+++ app/Wissance.Zerial/Wissance.Zerial.Installer/Zerial/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/Wissance/Zerial/raw/master/app/Wissance.Zerial/Wissance.Zerial.Installer/Windows/Wissance.Zerial.Win.X86.exe' # download url, HTTPS preferred
-$url64      = 'https://github.com/Wissance/Zerial/raw/master/app/Wissance.Zerial/Wissance.Zerial.Installer/Windows/Wissance.Zerial.Win.X64.exe' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$url        = 'https://github.com/Wissance/Zerial/raw/master/app/Wissance.Zerial/Wissance.Zerial.Installer/Windows/Wissance.Zerial.WinInstaller.Bootstrap.X86.exe' # download url, HTTPS preferred
+$url64      = 'https://github.com/Wissance/Zerial/raw/master/app/Wissance.Zerial/Wissance.Zerial.Installer/Windows/Wissance.Zerial.WinInstaller.Bootstrap.X64.exe' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -12,9 +12,9 @@ $packageArgs = @{
 
   softwareName  = 'Zerial'
 
-  checksum      = 'A42CBA54A45CD340F66F91B1DDD2D53BA8DC9318C06A4F9CC8BECD5BEA62F677'
+  checksum      = 'D4ACDFABEA3210D5EA103E9D2DE284C2271E575C141A6F11E53E546C38B5A81B'
   checksumType  = 'sha256'
-  checksum64    = 'D2B49D854F19730E96D6AF2CA38CFA4C3D81CEC4E41FACB5A715809531F6550F'
+  checksum64    = '3490E64CFA63AA767B55D78F63B3BE10A7C4900F3D0E486B3207BBBB1EB31129'
   checksumType64= 'sha256'
 
   silentArgs    = "/SILENT /qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
