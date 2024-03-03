f623d72a4747a36ff912f9b6299300df4bcb27bd
Ushakov Michael <um.nix.user@gmail.com>
2024-03-03 21:50:48 +0500

    Common method 4 getting port number from device name && test project

---
+++ app/Wissance.Zerial/Wissance.Zerial.Common.Tests/Utils/TestSerialPortNameHelper.cs
@@ -0,0 +1,13 @@
+namespace Wissance.Zerial.Common.Tests.Utils
+{
+    public class TestSerialPortNameHelper
+    {
+        public void TestBuildSerialDeviceName()
+        {
+        }
+        
+        public void TestGetSerialDeviceNumber()
+        {
+        }
+    }
+}
--- app/Wissance.Zerial/Wissance.Zerial.Common/Utils/SerialPortNameHelper.cs
+++ app/Wissance.Zerial/Wissance.Zerial.Common/Utils/SerialPortNameHelper.cs
@@ -28,28 +28,59 @@ namespace Wissance.Zerial.Common.Utils
 
         public static int GetSerialDeviceNumber(string deviceName)
         {
-            if (OperatingSystem.IsWindows())
+            try
             {
-                int portNumber = 0;
-                bool portParseResult = int.TryParse(deviceName.Substring("COM".Length), out portNumber);
-                if (portParseResult)
-                    return portNumber;
-                return -1;
-            }
-
-            if (OperatingSystem.IsLinux())
-            {
-                if (deviceName.Contains("USB"))
+                if (OperatingSystem.IsWindows())
                 {
+                    int portNumber = 0;
+                    bool portParseResult = int.TryParse(deviceName.Substring(WindowsComDeviceNameSign.Length),
+                        out portNumber);
+                    if (portParseResult)
+                        return portNumber;
+                    return -1;
                 }
-            }
 
-            return -1;
+                if (OperatingSystem.IsLinux())
+                {
+                    string deviceNumber = "";
+                    // name is /dev/ttyUSB{N}, where N - number 
+                    if (deviceName.Contains(LinuxUsbDeviceNameSign))
+                    {
+                        int nameSignStartIndex = deviceName.IndexOf(LinuxUsbDeviceNameSign);
+                        deviceNumber = deviceName.Substring(nameSignStartIndex + LinuxUsbDeviceNameSign.Length);
+                    }
+                    else
+                    {
+                        if (deviceName.Contains(LinuxComDeviceNameSign))
+                        {
+                            int nameSignStartIndex = deviceName.IndexOf(LinuxComDeviceNameSign);
+                            deviceNumber = deviceName.Substring(nameSignStartIndex + LinuxComDeviceNameSign.Length);
+                        }
+                    }
+                    
+                    int portNumber = 0;
+                    bool portParseResult = int.TryParse(deviceName.Substring(WindowsComDeviceNameSign.Length),
+                        out portNumber);
+                    if (portParseResult)
+                        return portNumber;
+                    return -1;
+
+                }
+
+                return -2;
+            }
+            catch (Exception)
+            {
+                return -255;
+            }
         }
 
-        private const string WindowsComPortNamePattern = "COM{0}";
+        private const string WindowsComDeviceNameSign = "COM";
+        private const string LinuxComDeviceNameSign = "tty";
+        private const string LinuxUsbDeviceNameSign = "ttyUSB";
+        private const string WindowsComPortNamePattern = $"{WindowsComDeviceNameSign}{{0}}";
         private const string LinuxSerialDeviceNamePattern = "/dev/{0}";
-        private const string LinuxComDeviceNamePattern = "tty{0}";
-        private const string LinuxUsbDeviceNamePattern = "ttyUSB{0}";
+        private const string LinuxComDeviceNamePattern = $"{LinuxComDeviceNameSign}{{0}}";
+        private const string LinuxUsbDeviceNamePattern = $"{LinuxUsbDeviceNameSign}{{0}}";
     }
 }
